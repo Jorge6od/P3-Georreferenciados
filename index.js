9c619836874bd574dde94248fdc83c7a1b336d19
Jorge6od <tonyrz2111@gmail.com>
2025-11-13 19:14:18 -0600

    corrigiendo proyecto

--- index.js
+++ index.js
@@ -6,40 +6,35 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 setupSwagger(app);
 
-
+// Routers
 const productsRouter = require('./routes/productsRouter');
 const usersRouter = require('./routes/usersRouter');
 const categoriesRouter = require('./routes/categoriesRouter');
 const brandsRouter = require('./routes/brandsRouter');
 
-mongoose.connect(
-  'mongodb+srv://jorge:bs781378@georrecluster.tloouky.mongodb.net/?retryWrites=true&w=majority&appName=georreCluster'
-)
+// Conexión a Mongo (VARIABLE DE ENTORNO)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Conectado a MongoDB'))
-  .catch(err => console.error('No se pudo conectar a MongoDB:', err));
+  .catch(err => console.error('Error al conectar MongoDB:', err));
 
-app.listen(4000, () => {
-});
-
-
-
+// Rutas
 app.use('/products', productsRouter);
 app.use('/users', usersRouter);
 app.use('/categories', categoriesRouter);
 app.use('/brands', brandsRouter);
 
-app.use(logErrors);
-app.use(errorHandler);
-
-
 app.get('/', (req, res) => {
   res.send('Hola');
 });
 
+// Manejo de errores
+app.use(logErrors);
+app.use(errorHandler);
 
-
+// ÚNICO app.listen
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
