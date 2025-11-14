4e980ade5497a4b7afda48371338486267adb0c0
Jorge6od <tonyrz2111@gmail.com>
2025-11-13 19:22:47 -0600

    corrigiendo proyecto

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const setupSwagger = require('./swagger');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+// Middlewares
 app.use(express.json());
 app.use(cors());
 setupSwagger(app);
@@ -15,8 +16,11 @@ const usersRouter = require('./routes/usersRouter');
 const categoriesRouter = require('./routes/categoriesRouter');
 const brandsRouter = require('./routes/brandsRouter');
 
-// Conexión a Mongo (VARIABLE DE ENTORNO)
-mongoose.connect(process.env.MONGODB_URI)
+// Conexión a Mongo (local + producción)
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  'mongodb+srv://jorge:bs781378@georrecluster.tloouky.mongodb.net/?retryWrites=true&w=majority&appName=georreCluster'
+)
   .then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.error('Error al conectar MongoDB:', err));
 
@@ -30,11 +34,11 @@ app.get('/', (req, res) => {
   res.send('Hola');
 });
 
-// Manejo de errores
+// Middlewares de errores
 app.use(logErrors);
 app.use(errorHandler);
 
-// ÚNICO app.listen
+// Levantar servidor (local + producción)
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
